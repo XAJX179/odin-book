c5846b23d02a6a79c17d613d7398193b88b97bf2
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-03-04 07:14:27 +0530

    feat(user): login using either email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: %i[github discord]
 
+  attr_writer :login
+
+  def login
+    @login || name || email
+  end
+
   def self.from_omniauth(auth)
     find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name # assuming the user model has a name
+      user.name = auth.info.name + "_from_#{auth.provider}" # assuming the user model has a name
       user.skip_confirmation!
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where([ "lower(name) = :value OR lower(email) = :value", { value: login.downcase } ]).first
+    elsif conditions.has_key?(:name) || conditions.has_key?(:email)
+      where(conditions.to_h).first
     end
   end
 
@@ -21,6 +36,6 @@ class User < ApplicationRecord
   has_many :post_likes, dependent: :destroy, inverse_of: :author
   has_many :post_comments, dependent: :destroy, inverse_of: :author
 
-  validates :name, presence: true, length: { within: 3..25 }, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { within: 3..25 }, uniqueness: { case_sensitive: false }, format: { with: /^[a-zA-Z0-9_.]*$/, multiline: true }
   validates :password, presence: true, length: { within: 8..100 }
 end
