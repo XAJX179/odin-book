687b8dced6e0529932b305735b2087e6a02b899c
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-03-27 23:22:23 +0530

    feat(app): accept, reject and cancel freind requests

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -4,10 +4,72 @@ class FriendRequestsController < ApplicationController
   def index; end
 
   def show; end
-  def new; end
-  def create; end
 
-  def destroy; end
+  def new
+    @friend_request = FriendRequest.new
+  end
+
+  def create
+    @friend_request = FriendRequest.new
+
+    begin
+      user = User.find_by!(name: params["friend_request"]["to"])
+    rescue ActiveRecord::RecordNotFound
+      flash.now.alert = "User not found!"
+      render :new, status: :not_found
+      return
+    end
+
+    existing_reqs = FriendRequest.between(current_user, user)
+    if current_user.friends.include? user
+      flash.now.alert = "Already friended!"
+      render :new, status: :unprocessable_content
+    elsif existing_reqs.exists?
+      flash.now.alert = "your already sent or received a request!"
+      render :new, status: :unprocessable_content
+    else
+      @friend_request = current_user.outgoing_friend_requests.build(to: user, status: :pending)
+      if @friend_request.save
+        flash.now.notice = "Friend request sent!"
+        respond_to do |format|
+          format.turbo_stream { head :ok }
+        end
+      else
+        flash.now.alert = "Friend request invalid! could not be created!"
+        render :new, status: :unprocessable_content
+      end
+    end
+  end
+
+  def destroy
+    begin
+      @friend_request = FriendRequest.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash.now.alert = "Friend request not found"
+      render :index, status: :not_found
+      return
+    end
+
+    if @friend_request.to == current_user || @friend_request.from == current_user
+      @friend_request.destroy
+    else
+      flash.now.alert = "Not allowed to delete other's friend request"
+      render :index, status: :forbidden
+      return
+    end
+
+    if @friend_request.destroyed?
+      respond_to do |format|
+        format.turbo_stream do
+          flash.now.notice = "Friend request destroyed !"
+          head :ok
+        end
+      end
+    else
+      flash.now.alert = "Server Error"
+      render :index, status: :internal_server_error
+    end
+  end
 
   def incoming; end
 
@@ -36,6 +98,27 @@ class FriendRequestsController < ApplicationController
       else
         format.turbo_stream { render "load_outgoing" }
       end
+    end
+  end
+
+  def accept
+    begin
+      @friend_request = FriendRequest.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash.now.alert = "Friend request not found"
+      render :index, status: :not_found
+      return
+    end
+
+    if @friend_request.to == current_user
+      flash.now.notice = "New friend added!"
+      Friendship.find_or_create_by(user: current_user, friend: @friend_request.from)
+      Friendship.find_or_create_by(user: @friend_request.from, friend: current_user)
+      @friend_request.destroy
+      @friend_request.opposite_request&.destroy
+    else
+      flash.now.alert = "Not allowed to accept other's incoming friend request"
+      render :index, status: :forbidden
     end
   end
 end
